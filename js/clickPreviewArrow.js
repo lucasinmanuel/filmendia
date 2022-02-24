c228b981bb31137816c38f87425fb38e4b9a7cea
Lucas Emanuel Santana dos Santos <lucasemanuel2077@gmail.com>
2022-02-24 19:58:50 -0300

    upload geral

---
+++ js/clickPreviewArrow.js
@@ -0,0 +1,13 @@
+const previewArrow = document.querySelectorAll('.previewPopular-texts span')
+
+previewArrow[0].addEventListener('click',()=>{
+    //SETA ESQUERDA
+})
+
+previewArrow[1].addEventListener('click',()=>{
+    //SETA DIREITA
+    
+})
+
+
+
