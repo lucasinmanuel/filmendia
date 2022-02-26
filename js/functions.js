b4cea07aa0931cc96c7845de9f59a4ea43f4588f
Lucas Emanuel Santana dos Santos <lucasemanuel2077@gmail.com>
2022-02-26 13:57:35 -0300

    upload

--- js/functions.js
+++ js/functions.js
@@ -4,47 +4,85 @@ window.onload = () => {
         method: 'GET'
     })
     .then(response => response.json())
-    .then(function(json){
-        const previewImages = document.querySelector('.preview-images')
-        json.results.slice(0,18).map(async(val)=>{
+    .then((jsonPopular)=>{
+
+        const previewPopularImages = document.querySelector('.previewPopular')
+        jsonPopular.results.slice(0,18).map((val)=>{
             
-            previewImages.innerHTML += `
+            previewPopularImages.innerHTML += `
             
                 <div class="preview-single">
                     <img src="https://image.tmdb.org/t/p/w185${val.poster_path}" />
                 </div>
             
             `
-            const qtdPreviewSingle = document.querySelectorAll('.preview-single').length
-            console.log(qtdPreviewSingle)
+
         })
+
+        const qtdPreviewSingle = document.querySelectorAll('.preview-single').length
+        initSliderBullets(qtdPreviewSingle)
         
+    })
+
+    fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=d9006a76b9606894cb5d01eda1af5904&language=pt-br', {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then((jsonTopRated)=>{
+
+        const previewTopRatedImages = document.querySelector('.previewTopRated')
+        jsonTopRated.results.slice(0,18).map((val)=>{
+
+            previewTopRatedImages.innerHTML += `
+            
+                <div class="preview-single">
+                    <img src="https://image.tmdb.org/t/p/w185${val.poster_path}" />
+                </div>
+            
+            `
+
+        })
+
     })
     
     clickPreviewArrowns()
+    var click = 0
+
+    function initSliderBullets(val){
+        const previewBullets = document.querySelector('.previewSlider-bullets')
+        for(var i = 0;i < val / 6;i++){
+            previewBullets.innerHTML += '<span></span>'
+        }
+        const previewBulletSingle = document.querySelectorAll('.previewSlider-bullets span')
+        previewBulletSingle[click].style.backgroundColor = '#ccc'
+    }
 
     function clickPreviewArrowns(){
 
-        var click = 0
+        const previewArrow = document.querySelectorAll('.preview-texts span.setaSpan')
+        const previewImages = document.querySelectorAll('.preview-images')
+        const qtdPreviewImages = () => {
+            if(previewImages.length != 1){
+                return previewImages.length * 6
+            }else{
+                return 6
+            }
+        }
 
-        const sliderBullets = document.querySelector('.slider-bullets')
-        
-        /*for(var i = 0;i < qtdPreviewSingle / 6;i++){
-            sliderBullets.innerHTML += 'a'
-            
-        }*/
-        
-        const previewArrow = document.querySelectorAll('.preview-texts span')
-        const previewImages = document.querySelector('.preview-images')
-    
         previewArrow[0].addEventListener('click',()=>{
             //SETA ESQUERDA
-            if(click === 2){
-                previewImages.style.right = 1194+'px'
-                click = 1
-            }else if(click === 1){
-                previewImages.style.right = 0
-                click = 0
+            const qtdSliderBullets = document.querySelectorAll('.preview-single').length / qtdPreviewImages() - 1
+            for(let i = 0;i < previewImages.length;i++){
+            
+                if(previewImages[i].classList[1] === 'previewPopular'){
+                    if(click === qtdSliderBullets){
+                        previewImages[i].style.right = 1194+'px'
+                        click = 1
+                    }
+                }else if(previewImages[i].classList[1] === 'previewTopRated'){
+
+                }
+
             }
 
         })
@@ -52,15 +90,15 @@ window.onload = () => {
         previewArrow[1].addEventListener('click',()=>{
             //SETA DIREITA
             if(click === 0){
-                previewImages.style.right = 1194+'px'
+                previewImages[0].style.right = 1194+'px'
                 click = 1
             }else if(click === 1){
-                previewImages.style.right = 2388+'px'
+                previewImages[0].style.right = 2388+'px'
                 click = 2
             }
 
         })
-
+        
     }/*clickPreviewArrowns*/
        
 }
