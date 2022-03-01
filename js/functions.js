e12639ac725ed7d458b50979b10509fa73705d45
Lucas Emanuel Santana dos Santos <lucasemanuel2077@gmail.com>
2022-02-28 21:48:05 -0300

    upload

--- js/functions.js
+++ js/functions.js
@@ -14,32 +14,80 @@ window.onload = () => {
 
     function mandarGenresIds(genres){
 
-        //REQUISIÇÃO DOS FILMES MAIS POPULARES
-        fetch('https://api.themoviedb.org/3/movie/popular?api_key=d9006a76b9606894cb5d01eda1af5904&language=pt-br&page=1', {
-        method: 'GET'
+        var numberPage = 1
+
+        fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=d9006a76b9606894cb5d01eda1af5904&language=pt-br&page='+numberPage, {
+            method: 'GET'
         })
-        .then(response => response.json())
-        .then((jsonPopular)=>{
+        .then(response=>response.json())
+        .then(async(jsonNowPlaying)=>{
 
-            jsonPopular.results.slice(0,18).map((val)=>{
-      
-                var generosPopular = []
+            console.log(jsonNowPlaying)
+            console.log(genres)
+            var filmeIndex = 0
 
+            await jsonNowPlaying.results.forEach((val)=>{
+
+                var generos = []
+       
                 genres.forEach((value) => {
-            
+                   
                     for(let i = 0;i < val.genre_ids.length;i++){
-
+                        
                         if(val.genre_ids[i] === value.id){
-                            generosPopular.push(value.name)
-                        }else if(val.genre_ids[i] === value.id){
-                            generosPopular.push(value.name)
-                        }else if(val.genre_ids[i] === value.id){
-                            generosPopular.push(value.name)
+
+                            generos.push(value.name)
+    
                         }
                         
                     }
 
                 });
+
+                const bannerHomeMisterio = document.querySelector('.bannerMisterio-wrapper')
+
+                generos.forEach((value)=>{
+
+                    if(value === 'Ação'){
+                        console.log(value)
+                        if(filmeIndex < 3){
+                            
+                            bannerHomeMisterio.innerHTML += `
+
+                                <div class="bannerMisterio-images">
+                                    <img src="https://image.tmdb.org/t/p/w185${val.poster_path}" />
+                                </div>
+                                <div class="bannerMisterio-info">
+                                    <p>oiiiiiiiiiiiiiiiiiii</p>
+                                </div>
+
+                            `
+                            filmeIndex++
+
+                        }else{
+                            
+                            return false
+
+                        }
+                        
+                    }
+
+                })
+
+            })//JSON NOW_PLAYING PÁGINAS
+            
+            numberPage++
+            
+        })
+        
+        //REQUISIÇÃO DOS FILMES MAIS POPULARES
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=d9006a76b9606894cb5d01eda1af5904&language=pt-br&page=1', {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then((jsonPopular)=>{
+
+            jsonPopular.results.slice(0,18).map((val)=>{
 
                 const previewPopularImages = document.querySelector('.previewPopular-images')
     
